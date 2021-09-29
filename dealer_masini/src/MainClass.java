a291be76dfdda995db3b9a6e637a0d46be7868ab
noje_ <noje_i@yahoo.com>
2021-09-29 17:37:09 +0300

    commit

--- dealer_masini/src/MainClass.java
+++ dealer_masini/src/MainClass.java
@@ -12,11 +12,12 @@ public class MainClass
 
 
         int opt;
-        String sort_opt;
+        Character sort_opt;
+        Character buy_opt;
 
 
-
-        System.out.println("MENIU");
+        //inca un meniu pentru detinator
+        System.out.println("MENIU CUMPARATOR");
         System.out.println("1.Vedeti garaj(inventar)");
                            //sortare dupa diferite criterii
         System.out.println("2.Afisare informatii despre masina");
@@ -31,7 +32,24 @@ public class MainClass
         {
             case 1:
                 System.out.println("Doriti sa sortati?[Y/N]");
+                sort_opt = sc.next().charAt(0);
 
+                if(sort_opt == 'Y' || sort_opt == 'y')
+                {
+                    System.out.println("Sortati in functie de: ");
+                    System.out.println("1.Km");
+                    System.out.println("2.Culoare");
+                    System.out.println("3.Firma");
+                    System.out.println("4.Tip");
+                }
+                if(sort_opt == 'n' || sort_opt == 'N')
+                {
+                    //afisare toate masinile
+                }
+                else
+                {
+                    System.out.println("ati introdus litera gresita!");
+                }
                 break;
             case 2:
 
@@ -40,6 +58,19 @@ public class MainClass
 
                 break;
             case 4:
+
+                System.out.println("Doriti sa cumparati o masina?[Y/N]");
+                buy_opt = sc.next().charAt(0);
+
+                if(buy_opt == 'Y' || buy_opt == 'y')
+                {
+                    System.out.println("Sortati in functie de: ");
+                    System.out.println("1.Km");
+                    System.out.println("2.Culoare");
+                    System.out.println("3.Firma");
+                    System.out.println("4.Tip");
+                }
+
                 break;
 
 
