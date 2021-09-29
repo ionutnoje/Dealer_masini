8ee840974368fb22c0e9e842a9b66aac99c0088f
noje_ <noje_i@yahoo.com>
2021-09-29 16:58:07 +0300

    commit

---
+++ dealer_masini/src/MainClass.java
@@ -0,0 +1,17 @@
+public class MainClass
+{
+    public static void main(String[] args) {
+
+        System.out.println("MENIU");
+        System.out.println("1.Vedeti garaj(inventar)");
+        System.out.println("2.Afisare informatii despre masina");
+        System.out.println("3.Programare test drive");
+        System.out.println("4.Programare test drive");
+
+
+
+
+
+
+    }
+}
